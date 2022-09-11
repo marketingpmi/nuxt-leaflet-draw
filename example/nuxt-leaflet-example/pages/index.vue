10f36b8bb0d7a62e773e28547c63b7fdee824f17
nighthawale <info@marketingpmi.it>
2022-09-11 15:20:56 +0200

    disable toolbar

--- example/nuxt-leaflet-example/pages/index.vue
+++ example/nuxt-leaflet-example/pages/index.vue
@@ -3,6 +3,7 @@
     <div id="map-wrap" style="height: 100vh; width: 100%;">
       <no-ssr>
         <l-map :zoom="13" :center="[47.413220, -1.219482]">
+          <l-draw-toolbar position="topright"/>
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
           <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
         </l-map>
@@ -13,7 +14,7 @@
 <script>
 export default {
   mounted() {
-    
+
   }
 };
 </script>
